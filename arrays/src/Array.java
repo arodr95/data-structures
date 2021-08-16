44a4d6bff3e59645fbf7ea7bdc778a6155be7f24
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-16 11:52:48 -0500

    insert method

--- arrays/src/Array.java
+++ arrays/src/Array.java
@@ -1,13 +1,20 @@
 public class Array {
     private int[] items;
+    private int count;
 
     public Array(int length){
         items = new int[length];
+        count = 0;
     }
 
     public void print() {
         for(int i = 0; i < items.length; i++) {
             System.out.println(items[i]);
         }
+    }
+
+    public void insert(int value) {
+        items[count] = value;
+        count++;
     }
 }
