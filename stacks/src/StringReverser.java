9ba378ab9bf56a0713324ebe91a3f112ff5fea16
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-17 13:57:42 -0500

    use character array

--- stacks/src/StringReverser.java
+++ stacks/src/StringReverser.java
@@ -2,13 +2,14 @@ import java.util.Stack;
 
 public class StringReverser {
     public String reverse(String input) {
-        Stack<String> stack = new Stack<>();
-        for (int i = 0; i < input.length(); i++)
-            stack.push(input.substring(i, i + 1));
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : input.toCharArray())
+            stack.push(ch);
 
         var newString = "";
         while (!stack.empty())
-            newString += stack.pop();
+            newString += stack.pop().toString();
         return newString;
     }
 }
