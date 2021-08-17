4d10ab46691facda54f0070151478f807daf3fb7
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-17 14:05:17 -0500

    throw illegal argument exception when input is null

--- stacks/src/Main.java
+++ stacks/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        var string = "abcd";
+        var string = "";
 
         StringReverser reverser = new StringReverser();
-        var result = reverser.reverse(string);
+        var result = reverser.reverse(null);
         System.out.println(result);
     }
 }
