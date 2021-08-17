ff3e54e80bdd47f54c5481811751ac0196b3691e
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-17 15:47:58 -0500

    use arraylist to check bracket types and match brackets

--- stacks/src/Expression.java
+++ stacks/src/Expression.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
     private String input;
+    private final List<Character> leftBrackets = Arrays.asList('(', '{', '[', '<');
+    private final List<Character> rightBrackets = Arrays.asList(')', '}', ']', '>');
 
     public Expression(String string) {
         input = string;
@@ -13,15 +17,28 @@ public class Expression {
 
         Stack<Character> stack = new Stack<>();
         for(char ch : input.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[' || ch == '<')
+            if (isLeftBracket(ch))
                 stack.push(ch);
-            if(ch == ')' || ch == '}' || ch == ']' || ch == '>') {
+            if(isRightBracket(ch)) {
                 if (stack.empty()) return false;
+
                 var top = stack.pop();
-                if((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[') || (ch == '>' && top != '<')) return false;
+                if(!bracketsMatch(top, ch)) return false;
             }
         }
 
         return stack.empty();
+    }
+
+    private boolean isLeftBracket(char ch) {
+        return leftBrackets.contains(ch);
+    }
+
+    private boolean isRightBracket(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
     }
 }
