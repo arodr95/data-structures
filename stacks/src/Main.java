8df58629d10a3faf0eb996706b36ffd2bd0f7d2f
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-18 08:57:24 -0500

    two stacks class

--- stacks/src/Main.java
+++ stacks/src/Main.java
@@ -2,21 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayStack stack = new ArrayStack(3);
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
 
-        var item = stack.peek();
-
-        System.out.println(stack);
-        System.out.println(item);
-
-        while(!stack.isEmpty()){
-            System.out.println(stack);
-            System.out.println(stack.pop());
-        }
-
-        System.out.println(stack);
     }
 }
