512ff8e4e72dd3e685c4f5a7cc8590e8717065c7
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-18 10:07:39 -0500

    two stacks attempt

--- stacks/src/Main.java
+++ stacks/src/Main.java
@@ -2,6 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        TwoStacks items = new TwoStacks(5);
 
+        System.out.println(items);
+
+        items.push2(4);
+        items.push2(5);
+
+        var item = items.pop1();
+
+        System.out.println(item);
+        System.out.println(items);
     }
 }
--- stacks/src/TwoStacks.java
+++ stacks/src/TwoStacks.java
@@ -1,3 +1,77 @@
-public class TwoStacks {
+import java.util.Arrays;
+import java.util.Stack;
 
+public class TwoStacks {
+    private int[] items;
+    private int index1;
+    private int index2;
+
+    public TwoStacks(int size) {
+        items = new int[size];
+    }
+
+    public boolean isEmpty1() {
+        return index1 == 0;
+    }
+
+    public boolean isEmpty2() {
+        return index2 == index1;
+    }
+
+    private boolean isFull() {
+        return index2 > items.length;
+    }
+
+    public void push1(int item){
+        if (isFull())
+            throw new StackOverflowError();
+
+        shiftStack2Right();
+        items[index1++] = item;
+    }
+
+    public void push2(int item) {
+        if (isFull())
+            throw new StackOverflowError();
+
+        items[index2++] = item;
+    }
+
+    public int pop1() {
+        if (isEmpty1())
+            throw new IllegalStateException();
+
+        var item = items[index1 - 1];
+        index1--;
+        shiftStack2Left();
+        return item;
+    }
+
+    public int pop2() {
+        if (isEmpty2())
+            throw new IllegalStateException();
+
+        var item = items[index2 - 1];
+        index2--;
+        return item;
+    }
+
+    @Override
+    public String toString() {
+        var stack1 = Arrays.copyOfRange(items, 0, index1);
+        var stack2 = Arrays.copyOfRange(items, index1, index2);
+        return Arrays.toString(stack1) + Arrays.toString(stack2);
+    }
+
+    private void shiftStack2Right() {
+        for(int i = index2; i > index1; i--)
+            items[i] = items[i - 1];
+        index2++;
+    }
+
+    private void shiftStack2Left() {
+        index2--;
+        for(int i = index1; i < index2; i++)
+            items[i] = items[i+1];
+    }
 }
