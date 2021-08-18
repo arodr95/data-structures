06ae710b98527646b62c2d30df9250a09a98232b
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-18 08:46:43 -0500

    stack using linked list

--- stacks/src/Main.java
+++ stacks/src/Main.java
@@ -2,15 +2,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayStack stack = new ArrayStack(3);
+        LinkedListStack stack = new LinkedListStack();
         stack.push(10);
         stack.push(20);
         stack.push(30);
 
         var item = stack.peek();
 
-        System.out.println(item);
         System.out.println(stack);
+        System.out.println(item);
 
         while(!stack.isEmpty()){
             System.out.println(stack);
@@ -18,6 +18,5 @@ public class Main {
         }
 
         System.out.println(stack);
-
     }
 }
