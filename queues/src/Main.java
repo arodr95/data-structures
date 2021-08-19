9651f32e8361200c4c31128af79e68c9e4475036
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-19 09:44:06 -0500

    method to reverse a queue

--- queues/src/Main.java
+++ queues/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     public static void main(String args[]) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
 
+        reverse(queue);
+
+        System.out.println(queue);
+    }
+
+    public static void reverse(Queue<Integer> queue) {
+        var stack = new Stack<Integer>();
+        while(!queue.isEmpty())
+            stack.push(queue.remove());
+        while(!stack.isEmpty())
+            queue.add(stack.pop());
     }
 }
