86ce89ce3cce131ffa10e54304dc7e24fefc05f4
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-19 10:10:56 -0500

    build queue using array

--- queues/src/ArrayQueue.java
+++ queues/src/ArrayQueue.java
@@ -1,3 +1,47 @@
-public class ArrayQueue {
+import java.util.Arrays;
 
+public class ArrayQueue {
+    private int[] queue;
+    private int head;
+    private int tail;
+
+    public ArrayQueue(int size) {
+        queue = new int[size];
+        head = 0;
+        tail = 0;
+    }
+
+    public boolean isFull() {
+        return tail == queue.length;
+    }
+
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    public void enqueue(int item) {
+        if (isFull())
+            throw new StackOverflowError();
+
+
+
+        queue[tail++] = item;
+    }
+
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return queue[head++];
+    }
+
+    public int peek() {
+        return queue[head];
+    }
+
+    @Override
+    public String toString() {
+        var items = Arrays.copyOfRange(queue, head, tail);
+        return Arrays.toString(items);
+    }
 }
--- queues/src/Main.java
+++ queues/src/Main.java
@@ -4,7 +4,19 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String args[]) {
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
 
+        System.out.println(queue);
+
+        while(!queue.isEmpty()) {
+            System.out.println("current head is " + queue.peek());
+            System.out.println("removing (" + queue.dequeue() + ") ...");
+        }
     }
 
     public static void reverse(Queue<Integer> queue) {
