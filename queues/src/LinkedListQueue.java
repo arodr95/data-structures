733bb1388bfaefb56917edbfe54aecb9d29e170d
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-19 13:23:09 -0500

    implement queue using a LinkedList

--- queues/src/LinkedListQueue.java
+++ queues/src/LinkedListQueue.java
@@ -1,2 +1,35 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class LinkedListQueue {
+    // isEmpty, enqueue, dequeue, peek
+    private LinkedList<Integer> items;
+
+    public LinkedListQueue() {
+        items = new LinkedList<>();
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    public void enqueue(int item) {
+        items.addLast(item);
+    }
+
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return items.removeFirst();
+    }
+
+    public int peek() {
+        return items.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items.toArray());
+    }
 }
