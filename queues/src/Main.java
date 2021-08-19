b3038d9f68ee4a06cd667f904326ba3f32cfd9b9
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-19 11:29:56 -0500

    implement queue using stacks

--- queues/src/Main.java
+++ queues/src/Main.java
@@ -4,17 +4,17 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String args[]) {
-        ArrayQueue queue = new ArrayQueue(3);
-        queue.enqueue(1);
-        System.out.println(queue);
-        queue.enqueue(2);
-        System.out.println(queue);
-        System.out.println("removing (" + queue.dequeue() + ") ...");
-        queue.enqueue(3);
-        System.out.println(queue);
-        queue.enqueue(4);
-        System.out.println(queue);
-        System.out.println("removing (" + queue.dequeue() + ") ...");
+        StackQueue queue = new StackQueue();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+
+        while(!queue.isEmpty()){
+            System.out.println(queue);
+            System.out.println("top is " + queue.peek());
+            System.out.println("removing (" + queue.dequeue() + ") ...");
+        }
     }
 
     public static void reverse(Queue<Integer> queue) {
--- queues/src/StackQueue.java
+++ queues/src/StackQueue.java
@@ -1,2 +1,43 @@
+import java.util.Stack;
+
 public class StackQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public StackQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public void enqueue(int item) {
+        while(!stack2.isEmpty())
+            stack1.push(stack2.pop());
+
+        stack1.push(item);
+    }
+
+    public int dequeue() {
+        while(!stack1.isEmpty())
+            stack2.push(stack1.pop());
+
+        return stack2.pop();
+    }
+
+    public int peek() {
+        while(!stack1.isEmpty())
+            stack2.push(stack1.pop());
+
+        return stack2.peek();
+    }
+
+    @Override
+    public String toString() {
+        while(!stack2.isEmpty())
+            stack1.push(stack2.pop());
+        return stack1.toString();
+    }
 }
