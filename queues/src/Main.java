0c5f4cf706000b97f46cb9b9cd7c73d11e92eda9
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-19 09:44:36 -0500

    Create arrayqueue class

--- queues/src/Main.java
+++ queues/src/Main.java
@@ -4,14 +4,7 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String args[]) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
 
-        reverse(queue);
-
-        System.out.println(queue);
     }
 
     public static void reverse(Queue<Integer> queue) {
