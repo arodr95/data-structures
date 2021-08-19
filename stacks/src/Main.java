29d47a2546f5cd3d26d25ff6a6a9bbbec713ed7d
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-19 08:33:11 -0500

    minStack class and methods

--- stacks/src/Main.java
+++ stacks/src/Main.java
@@ -2,25 +2,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        TwoStacks items = new TwoStacks(6);
+        MinStack stack = new MinStack();
 
-        items.push1(1);
-        items.push1(2);
-        items.push1(3);
-        items.push2(4);
-        items.push2(5);
-        items.push2(6);
+        stack.push(20);
+        stack.push(10);
+        stack.push(5);
 
-        System.out.println(items);
-
-        while(!items.isEmpty1()){
-            System.out.println(items.pop1());
-            System.out.println(items);
-        }
-
-        while(!items.isEmpty2()){
-            System.out.println(items.pop2());
-            System.out.println(items);
+        while(!stack.isEmpty()){
+            System.out.println(stack);
+            System.out.println("minimum value is " + stack.min());
+            System.out.println("removing min (" + stack.pop() + ") ...");
+            System.out.println();
         }
     }
 }
