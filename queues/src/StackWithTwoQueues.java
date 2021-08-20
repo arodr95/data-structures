abbb791bbf071e48df9c9741f1b60ea74e731a44
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-20 09:10:29 -0500

    push and pop methods

--- queues/src/StackWithTwoQueues.java
+++ queues/src/StackWithTwoQueues.java
@@ -1,2 +1,27 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class StackWithTwoQueues {
+    Queue<Integer> queue1 = new ArrayDeque();
+    Queue<Integer> queue2 = new ArrayDeque();
+
+    // O(1)
+    public void push(int item) {
+        queue1.add(item);
+    }
+
+    // O(n)
+    public int pop() {
+        if (queue1.isEmpty())
+            throw new IllegalStateException();
+
+        while(queue1.size() > 1) {
+            queue2.add(queue1.remove());
+        }
+
+        var item = queue1.remove();
+        queue1 = queue2;
+
+        return item;
+    }
 }
