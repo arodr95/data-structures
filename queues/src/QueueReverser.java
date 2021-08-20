963a48adf9bb2f62be03c82c42db9156b7731c4e
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-20 08:08:21 -0500

    extract methods for readability

--- queues/src/QueueReverser.java
+++ queues/src/QueueReverser.java
@@ -1,5 +1,4 @@
 import java.util.ArrayDeque;
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -15,15 +14,25 @@ public class QueueReverser {
         if (k < 0 || k > queue.size())
             throw new IllegalArgumentException();
 
+        dequeue_K_ItemsToStack(k);
+        enqueue_K_ItemsFromStack();
+        moveItemsAfter_K_ToBack(k);
+    }
+
+    private void moveItemsAfter_K_ToBack(int k) {
+        for(int i = 0; i < queue.size() - k; i++)
+            queue.add(queue.remove());
+    }
+
+    private void enqueue_K_ItemsFromStack() {
+        while (!stack.isEmpty())
+            queue.add(stack.pop());
+    }
+
+    private void dequeue_K_ItemsToStack(int k) {
         for(int i = 0; i < k; i++) {
             stack.push(queue.remove());
         }
-
-        while (!stack.isEmpty())
-            queue.add(stack.pop());
-
-        for(int i = 0; i < queue.size() - k; i++)
-            queue.add(queue.remove());
     }
 
     @Override
