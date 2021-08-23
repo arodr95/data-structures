713b8fe2251b6fff56749f32f08036255ac366ad
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-23 15:11:07 -0500

    extract private methods for counting occurences and finding most frquent occurence

--- hash-tables/src/HashTableExercises.java
+++ hash-tables/src/HashTableExercises.java
@@ -6,19 +6,27 @@ public class HashTableExercises {
     // O(n)
     public int mostFrequent(int[] items) {
         Map<Integer, Integer> map = new HashMap<>();
+        countOccurences(items, map);
+
+        return findMostFrequentOccurence(map);
+    }
+
+    private int findMostFrequentOccurence(Map<Integer, Integer> map) {
+        int mostFrequent = -1;
+        int count = 0;
+        for(var entry : map.entrySet()) {
+            if(entry.getValue() > count) {
+                count = entry.getValue();
+                mostFrequent = entry.getKey();
+            }
+        }
+        return mostFrequent;
+    }
+
+    private void countOccurences(int[] items, Map<Integer, Integer> map) {
         for(var item : items) {
             var count = map.containsKey(item) ? map.get(item) : 0;
             map.put(item, count + 1);
         }
-
-        int mostFrquent = Integer.MIN_VALUE;
-        int count = Integer.MIN_VALUE;
-        for(var entry : map.entrySet()) {
-            if(entry.getValue() > count) {
-                count = entry.getValue();
-                mostFrquent = entry.getKey();
-            }
-        }
-        return mostFrquent;
     }
 }
