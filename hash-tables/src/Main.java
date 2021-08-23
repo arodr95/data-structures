7ee3f5d2760588c5e44617399b0e2ab1bc43ad83
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-23 07:34:21 -0500

    findFirstNonRepeatedCharacter attempt

--- hash-tables/src/Main.java
+++ hash-tables/src/Main.java
@@ -1,2 +1,30 @@
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public class Main {
+    public static void main(String args[]) {
+        // a green apple
+
+        var string = "a green apple";
+
+        System.out.println(findFirstNonRepeatedCharacter(string));
+    }
+
+    public static char findFirstNonRepeatedCharacter(String string){
+        var array = string.toLowerCase().toCharArray();
+        Map<Character, Boolean> map = new HashMap<>();
+
+
+        for(char character : array) {
+            if (map.containsKey(character)){
+                map.put(character,true);
+                return character;
+            } else {
+                map.put(character, false);
+            }
+        }
+
+        return '0';
+    }
 }
