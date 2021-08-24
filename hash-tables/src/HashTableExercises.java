3fc91b8f8bd161d10b802dd16432285dc3733c1d
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-24 12:36:12 -0500

    twoSum method and code refactoring/commenting

--- hash-tables/src/HashTableExercises.java
+++ hash-tables/src/HashTableExercises.java
@@ -7,14 +7,15 @@ public class HashTableExercises {
 
     // O(n)
     public int mostFrequent(int[] items) {
+        // Create a map containing each item and its frequency.
         Map<Integer, Integer> map = new HashMap<>();
-        countOccurrences(items, map);
+        for(var item : items) {
+            var count = map.containsKey(item) ? map.get(item) : 0;
+            map.put(item, count + 1);
+        }
 
-        return findMostFrequentOccurrence(map);
-    }
-
-    private int findMostFrequentOccurrence(Map<Integer, Integer> map) {
-        int mostFrequent = -1;
+        // Iterate over map to find item with the highest frequency.
+        int mostFrequent = Integer.MIN_VALUE;
         int count = 0;
         for(var entry : map.entrySet()) {
             if(entry.getValue() > count) {
@@ -22,24 +23,25 @@ public class HashTableExercises {
                 mostFrequent = entry.getKey();
             }
         }
+
         return mostFrequent;
     }
 
-    private void countOccurrences(int[] items, Map<Integer, Integer> map) {
-        for(var item : items) {
-            var count = map.containsKey(item) ? map.get(item) : 0;
-            map.put(item, count + 1);
-        }
-    }
-
     // O(n)
-    public int countPairsWithDiff(int[] items, int difference){
-        Set<Integer> set = new HashSet<>();
-        for(var item : items)
-            set.add(item);
+    public int countPairsWithDiff(int[] items, int difference) {
+        if (!containsAtLeastTwoItems(items))
+            throw new IllegalArgumentException();
 
+        // Extract all numbers to a set for quick lookup.
+        Set<Integer> set = new HashSet<>();
+        for (var num : items)
+            set.add(num);
+
+        // b = a + difference or b = a - difference
+        // Iterate over numbers again, and check if (num +||- difference) is in set.
+        // If so increment count and remove num to avoid re-counting the pair.
         var count = 0;
-        for(var num : set) {
+        for (var num : items) {
             if (set.contains(num + difference))
                 count++;
             if (set.contains(num - difference))
@@ -48,5 +50,30 @@ public class HashTableExercises {
         }
 
         return count;
+    }
+
+    // O(n)
+    public int[] twoSum(int[] items, int target) {
+        if (!containsAtLeastTwoItems(items))
+            throw new IllegalArgumentException();
+
+        // Create a map that will hold each value and its index.
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // a + b = target ---> target - a = b.
+        // On each iteration, if both numbers adding up to target are present
+        // return their indexes.
+        for(var index = 0; index < items.length; index++) {
+            int complement = target - items[index];
+            if (map.containsKey(complement))
+                return new int[] {map.get(complement), index};
+            map.put(items[index], index);
+        }
+
+        return null;
+    }
+
+    private boolean containsAtLeastTwoItems(int[] items) {
+        return items.length >= 2;
     }
 }
--- hash-tables/src/Main.java
+++ hash-tables/src/Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         HashTableExercises exercises = new HashTableExercises();
-        int[] items = new int[] {};
-        var k = 2;
-        System.out.println(exercises.countPairsWithDiff(items, k));
+        int[] items = new int[] {1, 2};
+        var target = 3;
+        System.out.println(Arrays.toString(exercises.twoSum(items, target)));
     }
 }
