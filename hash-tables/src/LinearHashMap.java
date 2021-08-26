f38bb68dd121c9bb6fd0774acce586a07ae7fd81
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-26 07:52:44 -0500

    extract insert into method and reduce logic

--- hash-tables/src/LinearHashMap.java
+++ hash-tables/src/LinearHashMap.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class LinearHashMap {
     private class Entry {
         private int key;
@@ -11,6 +14,7 @@ public class LinearHashMap {
 
     private Entry[] items;
     private int count;
+    private Set<Integer> keys = new HashSet<>();
 
     public LinearHashMap(int capacity) {
         items = new Entry[capacity];
@@ -26,22 +30,28 @@ public class LinearHashMap {
     }
 
     public void put(int key, String value) {
+        if (keys.contains(key))
+            throw new IllegalArgumentException("Key already exists.");
+
         var index = hash(key);
-        if (items[index] == null) {
-            items[index] = new Entry(key, value);
-            count++;
-            return;
-        }
-        else {
+        while (count < items.length) {
+            if (itemsIsNullAt(index)) {
+                insertAt(new Entry(key, value), index);
+                return;
+            }
             index++;
-            while (count < items.length)
-                if (items[hash(index)] == null) {
-                    items[index++] = new Entry(key, value);
-                    count++;
-                    return;
-                }
         }
 
-        throw new IllegalStateException();
+        throw new IllegalStateException("Map is at capacity.");
+    }
+
+    private void insertAt(Entry entry, int index) {
+        items[index] = entry;
+        keys.add(entry.key);
+        count++;
+    }
+
+    private boolean itemsIsNullAt(int index) {
+        return items[hash(index)] == null;
     }
 }
--- hash-tables/src/Main.java
+++ hash-tables/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String args[]) {
         LinearHashMap map = new LinearHashMap(3);
         map.put(0, "Andrew");
-        map.put(1, "Mosh");
         map.put(2, "Mosh");
+        map.put(3, "Mosh");
         System.out.println("Done");
     }
 }
