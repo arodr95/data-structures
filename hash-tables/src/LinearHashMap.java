a5ed2df48c252a82f612fd07506963b6c7046eef
Andrew Rodrigue <arodr95@lsu.edu>
2021-08-26 07:35:11 -0500

    put method

--- hash-tables/src/LinearHashMap.java
+++ hash-tables/src/LinearHashMap.java
@@ -10,6 +10,7 @@ public class LinearHashMap {
     }
 
     private Entry[] items;
+    private int count;
 
     public LinearHashMap(int capacity) {
         items = new Entry[capacity];
@@ -19,4 +20,28 @@ public class LinearHashMap {
     // get(int) -> string
     // remove(int)
     // size()
+
+    private int hash(int key) {
+        return key % items.length;
+    }
+
+    public void put(int key, String value) {
+        var index = hash(key);
+        if (items[index] == null) {
+            items[index] = new Entry(key, value);
+            count++;
+            return;
+        }
+        else {
+            index++;
+            while (count < items.length)
+                if (items[hash(index)] == null) {
+                    items[index++] = new Entry(key, value);
+                    count++;
+                    return;
+                }
+        }
+
+        throw new IllegalStateException();
+    }
 }
--- hash-tables/src/Main.java
+++ hash-tables/src/Main.java
@@ -2,7 +2,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        LinearHashMap map = new LinearHashMap(10);
+        LinearHashMap map = new LinearHashMap(3);
+        map.put(0, "Andrew");
+        map.put(1, "Mosh");
+        map.put(2, "Mosh");
         System.out.println("Done");
     }
 }
